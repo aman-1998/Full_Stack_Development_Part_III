15a3ce0a326b75df55295de864e0bea6be8c9a90
Aman Mishra <mishraamankpa@gmail.com>
2023-10-27 00:11:03 +0530

    Using Filter to validate user

---
+++ SpringBoot_SpringSec_part16_Filters_N113/src/main/java/personal/learning/config/MySpringSecurityConfig.java
@@ -0,0 +1,65 @@
+package personal.learning.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import personal.learning.config.auth.provider.CustomAuthenticationProvider;
+import personal.learning.filter.RequestValidationFilter;
+import personal.learning.services.UserService;
+import personal.learning.web.utility.Constants;
+
+@Configuration
+@EnableWebSecurity
+public class MySpringSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private UserService userService;
+	
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	/*
+	 * Custom Authentication Provider is required if you want to add some extra functionalities
+	 * other than username & password for authentication for eg. OTP, CAPTCHA, etc
+	 */
+	@Bean
+	public CustomAuthenticationProvider authenticationProvider() {
+		CustomAuthenticationProvider authProvider = new CustomAuthenticationProvider();
+		authProvider.setUserDetailsService(userService);
+		authProvider.setPasswordEncoder(passwordEncoder());
+		return authProvider;
+	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder authManagerBuilder) throws Exception {
+		authManagerBuilder.authenticationProvider(authenticationProvider());
+	}
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.addFilterBefore(new RequestValidationFilter(), UsernamePasswordAuthenticationFilter.class)
+			.authorizeRequests()
+			.antMatchers("/notice/**", "/holiday/**").authenticated()
+			.antMatchers("/performance/**").hasAuthority(Constants.ROLE_TEACHER)
+			.antMatchers("/pendingFees/**").hasAuthority(Constants.ROLE_ADMIN)
+			.and()
+			.formLogin()
+			.loginPage("/myLogin")
+			.loginProcessingUrl("/authenticateUser")
+			.permitAll()
+			.and()
+			.logout()
+			.permitAll()
+			.and()
+			.exceptionHandling().accessDeniedPage("/accessDenied");
+	}
+	
+}
---
+++ SpringBoot_SpringSec_part16_Filters_N113/src/main/java/personal/learning/filter/RequestValidationFilter.java
@@ -0,0 +1,40 @@
+package personal.learning.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.authentication.BadCredentialsException;
+
+public class RequestValidationFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		String username = req.getParameter("username");
+		try {
+			if(StringUtils.equals(username, "admin") 
+								|| StringUtils.equals(username, "master")) {
+				request.setAttribute("reservedUsername", "true");
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/view/loginPage.jsp");
+				dispatcher.forward( request, response );
+				throw new BadCredentialsException("Reserved username can't be used for login");
+			} else {
+				chain.doFilter(request, response);
+			}
+		} catch(BadCredentialsException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+}
